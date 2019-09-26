5a17b704a78694ca588d3605a44eba5e51328f45
Kevin Jobson <jobsonk2877@gmail.com>
2019-09-26 15:48:28 -0500

    choose gestures almost finished

--- RPSgame/RealPlayer.cs
+++ RPSgame/RealPlayer.cs
@@ -8,7 +8,7 @@ namespace RPSgame
     {
 
         // member variables (HAS A)
-   
+    
         // Constructor
         public RealPlayer()
         {
@@ -18,7 +18,42 @@ namespace RPSgame
         // member methods
         public override int ChooseGesture()
         {
+            do
+            {
+                string input;
+                Console.WriteLine("Decide Your move. Type: ROCK/PAPER/SCISSORS/LIZARD/SPOCK");
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "ROCK":
+                        choice = 1;
+                        break;
+                    case "PAPER":
+                        choice = 2;
+                        break;
+                        
+                    case "SCISSORS":
+                        choice = 3;
+                        break;
+                    case "LIZARD":
+                        choice = 4;
+                        break;
+                    case "SPOCK":
+                        choice = 5;
+                        break;
+                    default:
+                        Console.WriteLine("Please Enter A Valid Gesture");
+                        break;
+                }
+                while (choice != 1 && choice !=2 ) //finish later (1-5)  
+                return choice;
 
+
+
+
+
+
+            }
         }
         public override string NamePlayer()
         {
